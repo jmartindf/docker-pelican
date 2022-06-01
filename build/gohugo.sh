0e4f9b241f1fec04c89e5a41bbaebb887c94e8bc
Joe Martin <joe@bloomingcacti.com>
2022-06-01 16:15:02 -0700

    Properly install Hugo 0.100.1

--- build/gohugo.sh
+++ build/gohugo.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-if [ $ARCH = "amd64" ];
+HUGO_ARCH=""
+if [ ${TARGETPLATFORM} = "linux/amd64" ];
 then
 	HUGO_ARCH="Linux-64bit"
-elif [ $ARCH = "arm64v8" ];
+elif [ ${TARGETPLATFORM} = "linux/arm64" ];
 then
 	HUGO_ARCH="Linux-ARM64"
 fi
 HUGO_ID="hugo${HUGO_TYPE}_${HUGO_VERSION}_${HUGO_ARCH}"
-if [ $HUGO_ARCH -neq "" ];
+if [ "${HUGO_ARCH}" != "" ];
 then
-	curl  -o /tmp/hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_ID}.tar.gz"
-	tar -xf /tmp/hugo.tar.gz -C /tmp \
+	mkdir -p /tmp/hugo
+	wget -O /tmp/hugo/hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_ID}.tar.gz"
+	ls -lh /tmp/hugo
+	tar xzf /tmp/hugo/hugo.tar.gz -C /tmp/hugo \
 	  && mkdir -p /usr/local/sbin \
-	  && mv /tmp/hugo /usr/local/sbin/hugo \
-	  && rm -rf /tmp/${HUGO_ID}_linux_amd64 \
-	  && rm -rf /tmp/hugo.tar.gz \
-	  && rm -rf /tmp/LICENSE.md \
-	  && rm -rf /tmp/README.md
+	  && mv /tmp/hugo/hugo /usr/local/sbin/hugo \
+	  && rm -rf /tmp/hugo
 fi
